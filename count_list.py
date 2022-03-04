9a370c86aaf95346f30c5170587276bcebb6df77
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-04 18:45:02 +0200

    feat:add destructure variable

--- count_list.py
+++ count_list.py
@@ -1,13 +1,20 @@
 import re
 
 
-def count_list(parameter,count=0):
-    for i in parameter:
-        count+=1
-    return count
+def count_list(parameter,par2=False,count=0):
+    if par2 == True:
+        typ=type(parameter[0])
+        for i in parameter:
+            count+=1
+        return count, typ
+
+    else:    
+        for i in parameter:
+            count+=1
+        return count
 
 j=[2,3,4,5,6,7,5,0] 
-print(count_list(j))   
+print(count_list(j,True,-1))   
 k=[2,3,5,0]  
 print(count_list(k))   
 
@@ -16,5 +23,9 @@ print(count_list(n))
 
 m=[2,3]  
 print(count_list(m))   
-print(count_list('stroka list'))   
+print(count_list('stroka list')) 
+
+h,p=count_list(j,True,-1)
+print(h)
+print(p)
 
